e3a0ccd505eb90f8e0f66ec2ee6aefbe87c30c3e
liudan1166 <liudan1166@126.com>
2019-02-03 15:20:03 +0800

    项目修改

---
+++ supms-system/src/views/accountAdd/accountAdd.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="accountadd">
+        账户添加
+          <router-view/>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less">
+
+</style>
+
+
